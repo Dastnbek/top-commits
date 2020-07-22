06f4f6a5e9f93deaf16961b76076689f45f0e40a
Dastnbek <dostonbek.oripjonov@mail.ru>
2020-07-22 16:51:40 +0500

    loader added

--- src/components/App.js
+++ src/components/App.js
@@ -6,23 +6,27 @@ import CustomCard from "./card";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Loader from "./loader";
 
 import commitsApi from "../api";
 
 const App = () => {
   const [publicUsers, setPublicUsers] = useState([]);
   const [privateUsers, setPrivateUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const [mode, setMode] = useState("public");
   const getTopUsers = async () => {
     const result = await commitsApi.getUsers;
-
+    setLoading(false);
     setPublicUsers(result.data.users.users);
     setPrivateUsers(result.data.users.private_users);
   };
 
   const handleChange = (val) => {
+    setLoading(true);
     setMode(val.target.value);
+    setTimeout(() => setLoading(false), 1000);
   };
   useEffect(() => {
     getTopUsers();
@@ -63,31 +67,35 @@ const App = () => {
           </Grid>
         </Grid>
 
-        <Grid container spacing={1}>
-          {mode === "public"
-            ? publicUsers.map((user) => (
-                <Grid item xs={4}>
-                  <CustomCard
-                    name={user.login}
-                    company={user.company}
-                    contributions={user.contributions}
-                    img={user.avatarUrl}
-                    rank={user.rank}
-                  />
-                </Grid>
-              ))
-            : privateUsers.map((user) => (
-                <Grid item xs={4}>
-                  <CustomCard
-                    name={user.login}
-                    company={user.company}
-                    contributions={user.contributions}
-                    img={user.avatarUrl}
-                    rank={user.rank}
-                  />
-                </Grid>
-              ))}
-        </Grid>
+        {loading ? (
+          <Loader loading={loading} />
+        ) : (
+          <Grid container spacing={1}>
+            {mode === "public"
+              ? publicUsers.map((user) => (
+                  <Grid item xs={4}>
+                    <CustomCard
+                      name={user.login}
+                      company={user.company}
+                      contributions={user.contributions}
+                      img={user.avatarUrl}
+                      rank={user.rank}
+                    />
+                  </Grid>
+                ))
+              : privateUsers.map((user) => (
+                  <Grid item xs={4}>
+                    <CustomCard
+                      name={user.login}
+                      company={user.company}
+                      contributions={user.contributions}
+                      img={user.avatarUrl}
+                      rank={user.rank}
+                    />
+                  </Grid>
+                ))}
+          </Grid>
+        )}
       </Container>
     </>
   );
