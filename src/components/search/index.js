bb427de0356e46057bf2816675eceb9860d04e1b
Dastnbek <dostonbek.oripjonov@mail.ru>
2020-07-22 14:41:43 +0500

    Search is ready

---
+++ src/components/search/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles((theme) => ({
+  search: {
+    position: "relative",
+
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  inputRoot: {
+    background: "#5398DD",
+    border: "1px solid #5398DD",
+    borderRadius: "5px",
+    color: "white",
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
+    zIndex: 2,
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "30ch",
+    },
+  },
+}));
+
+const CustomSearch = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.search}>
+      <div className={classes.searchIcon}>
+        <SearchIcon />
+      </div>
+
+      <InputBase
+        placeholder="Search for GitHub user"
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ "aria-label": "search" }}
+      />
+    </div>
+  );
+};
+
+export default CustomSearch;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./components/card";
+import Search from "./components/search";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Card />
+    <Search />
   </React.StrictMode>,
   document.getElementById("root")
 );
